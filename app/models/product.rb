1c04a0ada1a56262cf24610e712e8739fb75fe26
Brandon Valle <bjvtamayo78@gmail.com>
2025-01-22 14:35:39 -0400

    Add the product model test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  validates :name, :default_price, presence: true
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe "Validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:default_price)}
+  end
+end
