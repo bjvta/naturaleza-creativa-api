d6095342a6127272d6950d93d467806bdaa68b08
Brandon Valle <bjvtamayo78@gmail.com>
2025-01-23 09:31:54 -0400

    Add the updateCustomer test as success

--- spec/graphql/mutations/update_customer_spec.rb
+++ spec/graphql/mutations/update_customer_spec.rb
@@ -6,7 +6,27 @@ RSpec.describe "UpdateCustomer Mutation", type: :request do
   let!(:customer) { create(:customer)}
 
   context 'when customer exists' do
+    it "should return the updated customer" do
+      query = <<GQL
+mutation {
+  updateCustomer(input: {
+    id: "#{customer.id}", name: "Jason"  
+  }){
+    customer {
+      id
+      name
+    }
+    errors
+  }
+}
+GQL
+      post '/graphql', params: { query: query}
+      result = JSON.parse(response.body)
 
+      expect(response).to have_http_status(:success)
+      expect(result['data']['updateCustomer']['customer']['name']).not_to eq(customer.name)
+      expect(result['data']['updateCustomer']['errors']).to be_nil
+    end
   end
 
   context 'when customer does not exist' do
